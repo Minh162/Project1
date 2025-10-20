2b98da1e96e9019d35e9e45112899fe42663e315
Minh162 <dndeodatduoc@gmail.com>
2025-10-20 22:33:38 +0700

    UpdateBase

---
+++ platformer-coop/platformer-coop/Prototype/player_prototype.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -200.0
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		if direction < 0:
+			animated_sprite_2d.flip_h = true
+		elif direction > 0:
+			animated_sprite_2d.flip_h = false
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	set_anim()
+	
+	move_and_slide()
+
+func set_anim() -> void:
+	if is_on_floor():
+		if velocity.x != 0:
+			animated_sprite_2d.play("move")
+		else:
+			animated_sprite_2d.play("idle")
+	else:
+		if velocity.y < 0:
+			animated_sprite_2d.play("jump")
+		elif velocity.y > 0:
+			animated_sprite_2d.play("fall")
