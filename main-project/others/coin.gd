626ff3514b587a552d1a2ca491b75ea78cbddb2f
kite1143 <70432793+kite1143@users.noreply.github.com>
2025-11-24 14:32:21 +0700

    Add sounds
    Add sounds for
    Goblin
    Fly
    Coin collecting

--- main-project/others/coin.gd
+++ main-project/others/coin.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collect_sound: AudioStreamPlayer2D = $CollectSound
 
 var is_picking_up = false
 
@@ -9,8 +10,9 @@ func _on_body_entered(body: Node2D) -> void:
 		return
 	
 	if body is PlayerCharacter:
+		GameManager.collect_coin()
+		collect_sound.play()
 		is_picking_up = true
 		animated_sprite_2d.play("pickup")
 		await animated_sprite_2d.animation_finished
 		queue_free()
-		GameManager.collect_coin()
